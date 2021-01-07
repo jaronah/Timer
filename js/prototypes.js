c653b44a28a5e7312c77831d974cd75de0ba9072
jaroslavnahodil <54673273+lattor@users.noreply.github.com>
2021-01-07 09:21:10 +0100

    added license notice

--- js/prototypes.js
+++ js/prototypes.js
@@ -1,10 +1,12 @@
 /** 
- * source: https://stackoverflow.com/questions/9790845/check-if-an-option-exist-in-select-element-without-jquery#answer-38225299
- * license: CC BY-SA 3.0
  * 
- * Modified into ES 2015
+ * code by Andy Mercer, (c) Stack Exchange Inc
+ * licensed under CC BY-SA 3.0 - https://creativecommons.org/licenses/by-sa/3.0/
+ * source: https://stackoverflow.com/a/38225299/14570657
  * 
- *  */ 
+ * Modified into ES6
+ * 
+ *  */
 HTMLSelectElement.prototype.contains = function (value) {
 
     for (let option of this.options) {
